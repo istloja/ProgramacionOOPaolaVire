8e6dc470f78ec6595fc8459a58e4724fcef874ff
Usuario <Usuario@Usuario-PC>
2019-07-09 22:56:31 -0500

    Sincronizacionde proyecto

---
+++ ProgramacionOOPaolaVire/src/programacionoopaolavire/ProgramacionOOPaolaVire.java
@@ -0,0 +1,10 @@
+
+package programacionoopaolavire;
+
+public class ProgramacionOOPaolaVire {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos gente");
+    }
+    
+}
